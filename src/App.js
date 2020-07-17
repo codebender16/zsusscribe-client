d60bb185e2ef81ec74d978fb1bf34259e96e5e13
CoderAcademy-MEL <angusl.zanshin@gmail.com>
2020-07-17 16:52:36 +1000

    added navbar, user login signup features

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import ProtectedRoute from './components/ProtectedRoute'
+import NavBar from './shared/NavBar'
+import { Route, Switch } from "react-router-dom";
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+import Home from './components/Home'
+import NoMatch from './components/NoMatch'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <NavBar />
+        <Switch>
+          <Route exact path="/sign-up" component={SignUp} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/" component={Home} />
+          <Route component={NoMatch} />
+        </Switch>
+      </>
+    );
+  }
 }
 
 export default App;
