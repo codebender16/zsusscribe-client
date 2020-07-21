57a9908cd972397d2c8d4b260d49fd446b0f7532
James Ma <allenbad@live.cn>
2020-07-21 19:41:06 +1000

    CRUD page create

--- src/shared/NavBar.jsx
+++ src/shared/NavBar.jsx
@@ -10,6 +10,7 @@ const NavBar = () => {
       <Link to="/">Home</Link>
       <Link to="/login">Login</Link>
       <Link to="/sign-up">Sign Up</Link>
+      <Link to="/secrets">Secrets</Link>
       <span
         onClick={() => {
           localStorage.removeItem("token");
