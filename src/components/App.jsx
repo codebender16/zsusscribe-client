3169268f177ee954d8ad28bd1b19c2d45e51b5d7
CoderAcademy-MEL <angusl.zanshin@gmail.com>
2020-07-22 12:34:44 +1000

    user login and sign up testing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ProtectedRoute from './components/ProtectedRoute'
+import ProtectedRoute from './ProtectedRoute'
 import NavBar from '../shared/NavBar'
 import { Route, Switch } from "react-router-dom";
 import Login from './Login'
