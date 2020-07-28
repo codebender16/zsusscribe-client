6d87208017ec4439e15ecee279a48eef8d47970d
James Ma <allenbad@live.cn>
2020-07-28 17:01:17 +1000

    fix the edit data show prob

--- src/components/CreateSubscription.jsx
+++ src/components/CreateSubscription.jsx
@@ -64,25 +64,25 @@ class CreateSubscription extends React.Component {
         <form className="w-full max-w-lg" onSubmit={this.onFormSubmit}>
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full px-3">
-              <label className="text-xl block tracking-wide text-gray-700 text-xs font-bold mb-2" for="name" htmlFor="name">Name</label>
+              <label className="text-xl block tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="name">Name</label>
               <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="eg) Netflix" name="name" id="name" onChange={this.onInputChange}/>          
             </div>
           </div>
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full px-3">    
-              <label className="text-xl block tracking-wide text-gray-700 text-xs font-bold mb-2 " for="payment-amount" htmlFor="payment_amount">Amount</label>
+              <label className="text-xl block tracking-wide text-gray-700 text-xs font-bold mb-2 " htmlFor="payment_amount">Amount</label>
               <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="AUD" name="payment_amount" id="payment_amount" onChange={this.onInputChange}/>
             </div>
           </div> 
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full px-3">  
-              <label className="text-xl block tracking-wide text-gray-700 text-xs font-bold mb-2 " for="category-name" htmlFor="category_name">Category:</label>
+              <label className="text-xl block tracking-wide text-gray-700 text-xs font-bold mb-2 " htmlFor="category_name">Category:</label>
               <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" name="category_name" id="category_name" onChange={this.onInputChange}/>
             </div>
           </div>
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full px-3">       
-              <label className="text-xl block tracking-wide text-gray-700 text-xs font-bold mb-2 " for="payment-date" htmlFor="payment_date">Payment Date</label>
+              <label className="text-xl block tracking-wide text-gray-700 text-xs font-bold mb-2 " htmlFor="payment_date">Payment Date</label>
               <DatePicker className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 selected={this.state.payment_date}
                 onChange={this.handleChange}
